f8013b346ed5e46b3cb5fdb63779ddd1c3416ef2
Timo Mätäsaho <timomata@ee.oulu.fi>
2013-08-27 14:41:46 +0300

    Added newline remover and put all the found matches to their respective lists in a single list where index of each sublist marks the linenumber of that line

--- OratUtils.py
+++ OratUtils.py
@@ -1,27 +1,38 @@
-#import codecs
 import re
 
 class OratUtils:
 
+	# Performs case sensitive search for text file tfile with string or character c on default.
+	# Argument c can be any regular expression
 	@staticmethod
 	def stringparser( tfile, c ):
 
-		string = []
+		lineindlist = [] # Holds lists of the match positions.
+		co = []
+		charcount = [] # Holds the lengths of each line
+
 		fid = open( tfile, 'r' )
 
-		string = fid.readline().decode('utf-8', 'ignore')
+		lines = fid.readlines()
 		fid.close()
 
-		print
-		print len(string)
-		print
-		print "testi " + string[63] + " testi "
-		print
+		
+		for string in lines:
+			
+			# Each string needs to be decoded to utf-8 as the files are saved in utf-8 format. 
+			# Without decoding matching would be done to ascii decoding and that causes the 
+			# strings to contain extra characters.
+			# Also the newline characters are removed so that the length of the lines are 
+			# correct
 
-		indlist = []
+			s = string.rstrip('\n').decode('utf-8', 'ignore')	
+			charindlist = []
+			charcount.append(len(s))
+			
+			for m in re.finditer(c, s):
+				charindlist.append(m.start())
 
-		for m in re.finditer(c, string):
-			indlist.append(m.start())	# m.start() osoittaa jokaisen osuman ekaan indeksiin.
-			#print m.start()
+			lineindlist.append(charindlist)
 
-		print indlist
+		print lineindlist
+		print charcount
